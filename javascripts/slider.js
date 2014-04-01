25a96665fa95160add15d51239c1b254075ea595
Chacha Sikes <chachasikes@gmail.com>
2014-04-01 12:18:33 -0700

    added comments and cleaned up javascript for slider.

--- javascripts/slider.js
+++ javascripts/slider.js
@@ -1,14 +1,15 @@
    
 $(document).ready(function() {
 
+  // Settings for the pricing slider.
   var priceSettings = {
     small: {
       population_start: 50,
       population_end: 50000,
       range_start: 1,
       range_end: 30,
-      range_subtract: 0,
-      price_base: 1000,
+      range_subtract: 0, // used for converting place on slider to population
+      price_base: 1000, // base price for level
       support_hours: 5,
       multiplier: 0.07
     },
@@ -41,31 +42,39 @@ $(document).ready(function() {
       price_base: 30000,
       support_hours: 150,
       multiplier: 0.03
-    },
+    }
   };
 
+  // Default price.
   var price = {
-      total_price: 0,
-      total_price_label: 0,
-      per_capita: 0,
-      population: 0,
-      population_label: 0,
-      price_base_label: 0,
-      plan_fee_label: 0,
-      price_5_years: 0
-    };
+    total_price: 0,
+    total_price_label: 0,
+    per_capita: 0,
+    population: 0,
+    population_label: 0,
+    price_base_label: 0,
+    plan_fee_label: 0,
+    price_5_years: 0
+  };
+
+  // Default fees.
+  var fees = {
+    'house_facts': 1000,
+    'partner': 5000,
+    'custom': 15000
+  };
 
 
-  function updateBox(size) {
-    if($('.category.' + size).hasClass('active') ) {
-      // Do nothing.
-    }
-    else {
+  // Highlight current city in css.
+  function highlightCity(size) {
+    // Only update if we are in a new range.
+    if(!$('.category.' + size).hasClass('active') ) {
       $('.category').removeClass('active');  
       $('.category.' + size).addClass('active');
     }    
-  };
+  }
 
+  // Figure out price based on position on slider.
   function determinePrice(value) {
       var range;
       var currentPrice = 0;
@@ -74,13 +83,14 @@ $(document).ready(function() {
         range = rangeMatch(value, size);
         if (range) { 
           currentPrice = priceFormula(range, value);
-          updateBox(size);
+          highlightCity(size);
         }
       }
       
       return currentPrice;
-  };
+  }
 
+  // Figure out which range we are in on the slider.
   function rangeMatch(value, size) {
     currentRange = priceSettings.small;
     
@@ -91,8 +101,9 @@ $(document).ready(function() {
     if (value >= currentRange.range_start && value <= currentRange.range_end ) {      
       return currentRange;
     }
-  };
+  }
 
+  // Update display of price in markup.
   function updatePriceDisplay(price) {
 
         $( "#price" ).val( "$" + price.per_capita);
@@ -101,9 +112,11 @@ $(document).ready(function() {
         $( "#price_base" ).val("$" + price.price_base_label );
         $( "#price_5_years" ).val("$" + price.price_5_years );
         // $( "#plan_fee" ).val("$" + price.plan_fee_label );
-  };
+  }
 
   // @TODO finish
+  // Add settings to URL bar so that price can be updated based on URL path.
+  // 
   // function updateURLParamsPricing(settings) {
   //   var settings = {
 
@@ -124,105 +137,109 @@ $(document).ready(function() {
   //   }
   // }
 
+  // Get current fee based on radio box selection.
   function getImplementationFee() {
     var selected = $("input[type='radio'][name='data_plan']:checked");
     var selectedVal = 0;
     if (selected.length > 0) {
         selectedVal = selected.val();
     }
-
-    var fees = {
-      'house_facts': 1000,
-      'partner': 5000,
-      'custom': 15000
-    }
-
     var fee;
     fee = fees[selectedVal];
     return fee;
   }
 
+  // Convert number to comma-separated number (for display.)
   function commaSeparateNumber(val){
     while (/(\d+)(\d{3})/.test(val.toString())){
       val = val.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
     }
     return val;
-  };
+  }
 
+  // Figure out pricing values to display.
   function priceFormula(range, value) {
-
     var currentPrice = 0;
     var currentPrice5Years = 0;
     var population = 0;
+    var currentPlanFee = 0;
 
-    var population  = Math.ceil((range.population_end * value ) / range.range_end);
-    console.log(population)
-;
 
+    // Population is calculated as the position on the slider times a multiplier
+    // (The range slider is smoothed out, but increments at the 'medium city' are bigger than smaller city range.)
+    population  = Math.ceil((range.population_end * value ) / range.range_end);
+    
+    // Figure out which plan is selected.
     currentPlanFee = getImplementationFee();
 
+    // If population is big, set the top end of the scale.
     if (range.population_start >= 800000) {
       currentPrice = range.price_base + (range.multiplier * 800000); 
       currentPrice5Years = range.price_base + (range.multiplier * 800000); 
-      //range.price_base + (currentPlanFee / 5) + (range.multiplier * 800000);  
     }
     else {
       currentPrice = range.price_base + (range.multiplier * population);  
       currentPrice5Years = range.price_base + (currentPlanFee / 5) + (range.multiplier * population);  
     }
 
-    // Per capita
+    // Determine price per capita
     if (population < 2 ) {
+      // Keep bottom end of range from zero'ing out and showing big numbers.
       price.per_capita = 0;
       price.price_5_years = 0;
     }
     else {
       price.per_capita = Number(currentPrice / population).toFixed(2);
+      
+      // Total price ever, including set up fee.
       price.price_5_years = Number(currentPrice5Years / population).toFixed(2);
     }
 
 
 
-
+    // Set values and put in object which can be rendered on the front end.
     price.population = population;
     price.total_price = Math.floor(currentPrice);
     price.total_price_label = commaSeparateNumber(Math.floor(currentPrice));
     price.population_label = commaSeparateNumber(population);
     price.plan_fee_label = currentPlanFee;
-
-
     price.price_base_label = commaSeparateNumber(currentPlanFee);
 
+    // Override value labels if population is large.
     if (range.population_start >= 800000) {
       price.population_label = "800,000 and up";
       price.total_price_label = commaSeparateNumber(Math.floor(currentPrice)) + "+";  
       price.price_base_label = commaSeparateNumber(currentPlanFee) + "+";
-      //price.price_5_years = price.total_price + (Math.floor(currentPlanFee / 5));
     }
 
-
     return price;
-  };
+  }
 
+  // Update price. 
   function updatePrice(value) {
+    var price;
     price = determinePrice(value);
     updatePriceDisplay(price);
-  };
+  }
 
 
-  var defaultPopulation = 50;
+  // ------------------------------------
+
+  // Set default range
+  var rangeDefault = 50;
+
+  // Set housefacts to be true by default.
   $("input[type='radio'][name='data_plan']").filter('[value="house_facts"]').attr('checked', true);
 
 
-
+  // Make radio box interactive.
   $("input[type='radio'][name='data_plan']").change(function(){
-    
     updatePrice($('#slider').slider("option", "value")  );
-
   });
 
+  // Make slider interactive.
   $( "#slider" ).slider({
-      value: defaultPopulation,
+      value: rangeDefault,
       min: 1,
       max: 120,
       step: 1,
@@ -232,8 +249,6 @@ $(document).ready(function() {
       }
     });
   
- 
-  updatePrice(defaultPopulation);
-
-
+  // Update default settings on page load.
+  updatePrice(rangeDefault);
 });
