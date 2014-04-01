f25513acc384fce7351132690362e11b7bd9f8ff
Chacha Sikes <chachasikes@gmail.com>
2014-03-31 19:23:28 -0700

    made some notes about updating URL params so that price settings can be 'saved'

--- javascripts/slider.js
+++ javascripts/slider.js
@@ -103,6 +103,27 @@ $(document).ready(function() {
         // $( "#plan_fee" ).val("$" + price.plan_fee_label );
   };
 
+  // @TODO finish
+  // function updateURLParamsPricing(settings) {
+  //   var settings = {
+
+  //   }
+  //   for (item in settings) {
+  //     updateQueryStringParameter('#pricing', key, value);
+  //   }
+  // }
+
+  // function updateQueryStringParameter(uri, key, value) {
+  //   var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+  //   var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+  //   if (uri.match(re)) {
+  //     return uri.replace(re, '$1' + key + "=" + value + '$2');
+  //   }
+  //   else {
+  //     return uri + separator + key + "=" + value;
+  //   }
+  // }
+
   function getImplementationFee() {
     var selected = $("input[type='radio'][name='data_plan']:checked");
     var selectedVal = 0;
