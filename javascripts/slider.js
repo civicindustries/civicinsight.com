81b905f8bfeb59606a43ab396a9cee7489e3cc3d
Chacha Sikes <chachasikes@gmail.com>
2014-03-31 18:54:42 -0700

    added implementation fee

--- javascripts/slider.js
+++ javascripts/slider.js
@@ -3,36 +3,40 @@ $(document).ready(function() {
 
   var priceSettings = {
     small: {
-      population_start: 0,
-      population_end: 49999,
+      population_start: 100,
+      population_end: 50000,
       range_start: 0,
       range_end: 10,
+      range_subtract: 0,
       price_base: 1000,
       support_hours: 5,
       multiplier: 0.07
     },
     medium: {
-      population_start: 50000,
-      population_end: 199999,
+      population_start: 50001,
+      population_end: 200000,
       range_start: 11,
+      range_subtract: 10,
       range_end: 20,
       price_base: 5000,
       support_hours: 50,
       multiplier: 0.05
     },
     large: {
-      population_start: 200000,
-      population_end: 799999,
+      population_start: 200001,
+      population_end: 800000,
       range_start: 21,
+      range_subtract: 20,
       range_end: 30,
       price_base: 15000,
       support_hours: 150,
       multiplier: 0.04
     },
     metro: {
-      population_start: 800000,
+      population_start: 800001,
       population_end: 1000000,
       range_start: 31,
+      range_subtract: 30,
       range_end: 40,
       price_base: 30000,
       support_hours: 150,
@@ -46,7 +50,9 @@ $(document).ready(function() {
       per_capita: 0,
       population: 0,
       population_label: 0,
-      price_base_label: 0
+      price_base_label: 0,
+      plan_fee_label: 0,
+      price_5_years: 0
     };
 
 
@@ -93,8 +99,27 @@ $(document).ready(function() {
         $( "#total_price" ).val( "$" + price.total_price_label );
         $( "#population" ).val(price.population_label );
         $( "#price_base" ).val("$" + price.price_base_label );
-
+        $( "#price_5_years" ).val("$" + price.price_5_years );
+        // $( "#plan_fee" ).val("$" + price.plan_fee_label );
   };
+
+  function getImplementationFee() {
+    var selected = $("input[type='radio'][name='data_plan']:checked");
+    var selectedVal = 0;
+    if (selected.length > 0) {
+        selectedVal = selected.val();
+    }
+
+    var fees = {
+      'house_facts': 1000,
+      'partner': 5000,
+      'custom': 15000
+    }
+
+    var fee;
+    fee = fees[selectedVal];
+    return fee;
+  }
 
   function commaSeparateNumber(val){
     while (/(\d+)(\d{3})/.test(val.toString())){
@@ -106,55 +131,84 @@ $(document).ready(function() {
   function priceFormula(range, value) {
 
     var currentPrice = 0;
-
+    var currentPrice5Years = 0;
     var population = 0;
-
     var rangeDifference = range.population_end - range.population_start;
-
     var rangeIncrement = rangeDifference / 10;
 
-    population = Math.floor(value * rangeIncrement);
+    population = Math.ceil((value - range.range_subtract) * rangeIncrement);
+
+
+    currentPlanFee = getImplementationFee();
 
     if (range.population_start >= 800000) {
-      currentPrice = range.price_base + (range.multiplier * 800000);  
+      currentPrice = range.price_base + (range.multiplier * 800000); 
+      currentPrice5Years =  range.price_base + (currentPlanFee / 5) + (range.multiplier * 800000);  
     }
     else {
       currentPrice = range.price_base + (range.multiplier * population);  
+      currentPrice5Years = range.price_base + (currentPlanFee / 5) + (range.multiplier * population);  
     }
 
     // Per capita
-    if (population == 0 ) {
+    if (population < 2 ) {
       price.per_capita = 0;
+      price.price_5_years = 0;
     }
     else {
       price.per_capita = Number(currentPrice / population).toFixed(2);
+      price.price_5_years = Number(currentPrice5Years / population).toFixed(2);
     }
+
+
+
 
     price.population = population;
     price.total_price = Math.floor(currentPrice);
-    price.total_price_label = commaSeparateNumber(Math.floor(currentPrice));
+    price.total_price_label = commaSeparateNumber(Math.floor(currentPrice))  + " per year.";
     price.population_label = commaSeparateNumber(population);
+    price.plan_fee_label = currentPlanFee;
 
 
-    price.price_base_label = commaSeparateNumber(range.price_base);
+
+
+
+
+
+
+
+    price.price_base_label = commaSeparateNumber(currentPlanFee) + " set up fee.";
 
     if (range.population_start >= 800000) {
       price.population_label = "800,000 and up";
       price.total_price_label = commaSeparateNumber(Math.floor(currentPrice)) + " and up";      
-      price.price_base_label = commaSeparateNumber(range.price_base) + " and up"
+      price.price_base_label = commaSeparateNumber(currentPlanFee) + " and up" + " setup fee."
+      price.price_5_years = price.total_price + (Math.floor(currentPlanFee / 5));
     }
 
 
     return price;
-  }
+  };
 
-
+  function updatePrice(value) {
+    price = determinePrice(value);
+    updatePriceDisplay(price);
+  };
 
 
   var defaultPopulation = 20;
+  $("input[type='radio'][name='data_plan']").filter('[value="house_facts"]').attr('checked', true);
+
+
+
+  $("input[type='radio'][name='data_plan']").change(function(){
+    
+    updatePrice($('#slider').slider("option", "value")  );
+
+  });
 
   $( "#slider" ).slider({
-      value:defaultPopulation,
+      value: defaultPopulation,
       min: 0,
       max: 40,
       step: 1,
@@ -164,8 +218,8 @@ $(document).ready(function() {
       }
     });
   
+ 
+  updatePrice(defaultPopulation);
 
-  price = determinePrice(defaultPopulation);
-  updatePriceDisplay(price);
 
 });
